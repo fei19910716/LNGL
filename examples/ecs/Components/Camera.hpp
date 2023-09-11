3821af397a3cb2328cbf38bda61b4bbd1cf65b9d
chenfei <chenfei@momenta.ai>
2023-09-11 22:47:11 +0800

    update ecs

--- examples/ecs/Components/Camera.hpp
+++ examples/ecs/Components/Camera.hpp
@@ -1,15 +1,18 @@
 #pragma once
 
-#include "Math/Mat44.hpp"
 #define _USE_MATH_DEFINES
 #include <cmath>
 
+#include <glm/glm.hpp>
+
+
+
 struct Camera
 {
-	Mat44 projectionTransform;
+	glm::mat4 projectionTransform;
 
 
-	static Mat44 MakeProjectionTransform(float fov, float nearClip, float farClip, unsigned int viewWidth, unsigned int viewHeight)
+	static glm::mat4 MakeProjectionTransform(float fov, float nearClip, float farClip, unsigned int viewWidth, unsigned int viewHeight)
 	{
 		float zClipBias0 =
 			(farClip + nearClip)
@@ -22,12 +25,12 @@ struct Camera
 		float zoomX = 1.0f / tanf((fov / 2.0f) * (M_PI / 180.0f));
 		float zoomY = (zoomX * static_cast<float>(viewWidth)) / static_cast<float>(viewHeight);
 
-		Mat44 transform;
-		transform.m[0][0] = zoomX;
-		transform.m[1][1] = zoomY;
-		transform.m[2][2] = -zClipBias0;
-		transform.m[3][2] = -1;
-		transform.m[2][3] = -zClipBias1;
+		glm::mat4 transform;
+		transform[0][0] = zoomX;
+		transform[1][1] = zoomY;
+		transform[2][2] = -zClipBias0;
+		transform[3][2] = -1;
+		transform[2][3] = -zClipBias1;
 
 		return transform;
 	}
