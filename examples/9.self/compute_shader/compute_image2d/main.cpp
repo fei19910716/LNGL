90fbcae2d1f2b9d64145256beda51df4152173ee
fec <fec@qti.qualcomm.com>
2023-09-20 10:12:32 +0800

    update compute shader

--- examples/9.self/compute_shader/compute_image2d/main.cpp
+++ examples/9.self/compute_shader/compute_image2d/main.cpp
@@ -1,23 +1,16 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-
-
-#include "base/shader_m.h"
-#include "base/shader_c.h"
-#include "base/camera.h"
+#include "base/utils.h"
 
 #include <iostream>
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void renderQuad();
 
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
+
+Shader *screenShader, *computeShader;
 
 // texture size
 const unsigned int TEXTURE_WIDTH = 1000, TEXTURE_HEIGHT = 1000;
@@ -26,134 +19,19 @@ const unsigned int TEXTURE_WIDTH = 1000, TEXTURE_HEIGHT = 1000;
 float deltaTime = 0.0f; // time between current frame and last frame
 float lastFrame = 0.0f; // time of last frame
 
-int main(int argc, char* argv[])
+
+// callbacks
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
-	// glfw: initialize and configure
-	// ------------------------------
-	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-	#ifdef __APPLE__
-		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-	#endif
-
-	// glfw window creation
-	// --------------------
-	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
-	if (window == NULL)
-	{
-		std::cout << "Failed to create GLFW window" << std::endl;
-		glfwTerminate();
-		return -1;
-	}
-	glfwMakeContextCurrent(window);
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-	glfwSwapInterval(0);
-
-	// glad: load all OpenGL function pointers
-	// ---------------------------------------
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-	{
-		std::cout << "Failed to initialize GLAD" << std::endl;
-		return -1;
-	}
-
-	// query limitations
-	// -----------------
-	int max_compute_work_group_count[3];
-	int max_compute_work_group_size[3];
-	int max_compute_work_group_invocations;
-
-	for (int idx = 0; idx < 3; idx++) {
-		glGetIntegeri_v(GL_MAX_COMPUTE_WORK_GROUP_COUNT, idx, &max_compute_work_group_count[idx]);
-		glGetIntegeri_v(GL_MAX_COMPUTE_WORK_GROUP_SIZE, idx, &max_compute_work_group_size[idx]);
-	}	
-	glGetIntegerv(GL_MAX_COMPUTE_WORK_GROUP_INVOCATIONS, &max_compute_work_group_invocations);
-
-	std::cout << "OpenGL Limitations: " << std::endl;
-	std::cout << "maximum number of work groups in X dimension " << max_compute_work_group_count[0] << std::endl;
-	std::cout << "maximum number of work groups in Y dimension " << max_compute_work_group_count[1] << std::endl;
-	std::cout << "maximum number of work groups in Z dimension " << max_compute_work_group_count[2] << std::endl;
-
-	std::cout << "maximum size of a work group in X dimension " << max_compute_work_group_size[0] << std::endl;
-	std::cout << "maximum size of a work group in Y dimension " << max_compute_work_group_size[1] << std::endl;
-	std::cout << "maximum size of a work group in Z dimension " << max_compute_work_group_size[2] << std::endl;
-
-	std::cout << "Number of invocations in a single local work group that may be dispatched to a compute shader " << max_compute_work_group_invocations << std::endl;
-
-	// build and compile shaders
-	// -------------------------
-	Shader screenQuad("screenQuad.vs", "screenQuad.fs");
-	ComputeShader computeShader("computeShader.cs");
-
-	screenQuad.use();
-	screenQuad.setInt("tex", 0);
-
-	// Create texture for opengl operation
-	// -----------------------------------
-	unsigned int texture;
-
-	glGenTextures(1, &texture);
-	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_2D, texture);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, TEXTURE_WIDTH, TEXTURE_HEIGHT, 0, GL_RGBA, GL_FLOAT, NULL);
-
-	glBindImageTexture(0, texture, 0, GL_FALSE, 0, GL_READ_WRITE, GL_RGBA32F);
-
-	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_2D, texture);
-
-	// render loop
-	// -----------
-	int fCounter = 0;
-	while (!glfwWindowShouldClose(window))
-	{
-		// Set frame time
-		float currentFrame = glfwGetTime();
-		deltaTime = currentFrame - lastFrame;
-		lastFrame = currentFrame;
-		if(fCounter > 500) {
-			std::cout << "FPS: " << 1 / deltaTime << std::endl;
-			fCounter = 0;
-		} else {
-			fCounter++;
-		}		
-
-		computeShader.use();
-		computeShader.setFloat("t", currentFrame);
-		glDispatchCompute((unsigned int)TEXTURE_WIDTH/10, (unsigned int)TEXTURE_HEIGHT/10, 1);
-
-		// make sure writing to image has finished before read
-		glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
-
-		// render image to quad
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		screenQuad.use();
-		
-		renderQuad();
-
-		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-		// -------------------------------------------------------------------------------
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	}
-
-	// optional: de-allocate all resources once they've outlived their purpose:
-	// ------------------------------------------------------------------------
-	glDeleteTextures(1, &texture);
-	glDeleteProgram(screenQuad.ID);
-	glDeleteProgram(computeShader.ID);
-
-	glfwTerminate();
-
-	return EXIT_SUCCESS;
+    glViewport(0, 0, width, height);
 }
+
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
 
 // renderQuad() renders a 1x1 XY quad in NDC
 // -----------------------------------------
@@ -186,11 +64,86 @@ void renderQuad()
 	glBindVertexArray(0);
 }
 
-// glfw: whenever the window size changed (by OS or user resize) this callback function executes
-// ---------------------------------------------------------------------------------------------
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+void init(GLFWwindow* window)
 {
-	// make sure the viewport matches the new window dimensions; note that width and 
-	// height will be significantly larger than specified on retina displays.
-	glViewport(0, 0, width, height);
+	// query limitations
+	// -----------------
+	int max_compute_work_group_count[3];
+	int max_compute_work_group_size[3];
+	int max_compute_work_group_invocations;
+
+	for (int idx = 0; idx < 3; idx++) {
+		glGetIntegeri_v(GL_MAX_COMPUTE_WORK_GROUP_COUNT, idx, &max_compute_work_group_count[idx]);
+		glGetIntegeri_v(GL_MAX_COMPUTE_WORK_GROUP_SIZE, idx, &max_compute_work_group_size[idx]);
+	}	
+	glGetIntegerv(GL_MAX_COMPUTE_WORK_GROUP_INVOCATIONS, &max_compute_work_group_invocations);
+
+	std::cout << "OpenGL Limitations: " << std::endl;
+	std::cout << "maximum number of work groups in X dimension " << max_compute_work_group_count[0] << std::endl;
+	std::cout << "maximum number of work groups in Y dimension " << max_compute_work_group_count[1] << std::endl;
+	std::cout << "maximum number of work groups in Z dimension " << max_compute_work_group_count[2] << std::endl;
+
+	std::cout << "maximum size of a work group in X dimension " << max_compute_work_group_size[0] << std::endl;
+	std::cout << "maximum size of a work group in Y dimension " << max_compute_work_group_size[1] << std::endl;
+	std::cout << "maximum size of a work group in Z dimension " << max_compute_work_group_size[2] << std::endl;
+
+	std::cout << "Number of invocations in a single local work group that may be dispatched to a compute shader " << max_compute_work_group_invocations << std::endl;
+
+	// build and compile shaders
+	// -------------------------
+	screenShader = new Shader;
+	screenShader->LoadShaderStage("screenQuad.vs",GL_VERTEX_SHADER);
+	screenShader->LoadShaderStage("screenQuad.fs",GL_FRAGMENT_SHADER);
+	screenShader->Link();
+
+	computeShader = new Shader;
+	computeShader->LoadShaderStage("compute.cs",GL_COMPUTE_SHADER);
+	computeShader->Link();
+
+	screenShader->use();
+	screenShader->SetUniform("tex", 0);
+
+	// Create texture for opengl operation
+	// -----------------------------------
+	unsigned int texture;
+
+	glGenTextures(1, &texture);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, TEXTURE_WIDTH, TEXTURE_HEIGHT, 0, GL_RGBA, GL_FLOAT, NULL);
+
+	glBindImageTexture(0, texture, 0, GL_FALSE, 0, GL_READ_WRITE, GL_RGBA32F);
+
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, texture);
 }
+
+void render()
+{
+
+	// Set frame time
+	float currentFrame = glfwGetTime();
+	deltaTime = currentFrame - lastFrame;
+	lastFrame = currentFrame;
+
+	computeShader->use();
+	computeShader->SetUniform("t", currentFrame);
+	glDispatchCompute((unsigned int)TEXTURE_WIDTH/10, (unsigned int)TEXTURE_HEIGHT/10, 1);
+
+	// make sure writing to image has finished before read
+	glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
+
+	// render image to quad
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	screenShader->use();
+	
+	renderQuad();
+}
+
+void clean(){}
+
+ExamleMain("compute_image2d", SCR_WIDTH, SCR_HEIGHT)
