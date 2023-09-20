03539d0f5dc20d5b2181655b36059fb2c02bd84e
fec <fec@qti.qualcomm.com>
2023-09-20 12:38:09 +0800

    update

--- examples/9.self/__particles_and_instancing/2.4.particles_compute_shader/2.3.particles_compute_shader.cpp
+++ examples/9.self/__particles_and_instancing/2.4.particles_compute_shader/2.3.particles_compute_shader.cpp
@@ -1,5 +1,17 @@
 
 
 /*
- * Particle System using Compute Shader： https://github.com/diharaw/gpu-particle-system
+ * Particle System using Compute Shader
+*/
+
+/**
+ * https://github.com/SaschaWillems/openglcpp/tree/master/computeShader
+*/
+
+/**
+ * https://github.com/Jax922/gpu-particle-system
+*/
+
+/**
+ * https://github.com/diharaw/gpu-particle-system
 */
